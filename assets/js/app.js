4b919161cea18d31184f4e3dd81c56e6c3420251
Tsuguma <htnod1429@gmail.com>
2018-11-05 00:06:56 +0900

    削除・doneボタン追加

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,11 +8,17 @@ let doneIcon = '<i class="far fa-check-circle fa-lg"></i>';
     let taskName = document.getElementById('task').value;
     let not = document.createElement('li');
     not.textContent = taskName;
-
-
-
-
-
+    let buttons =document.createElement('div');
+    buttons.classList.add('buttons');
+    let remove = document.createElement('button');
+    remove.classList.add('remove');
+    remove.innerHTML = removeIcon;
+    let done = document.createElement('button');
+    done.classList.add('done');
+    done.innerHTML = doneIcon;
+    buttons.appendChild(remove);
+    buttons.appendChild(done);
+    not.appendChild(buttons);
     document.getElementById('not-yet').appendChild(not);
 
   })
