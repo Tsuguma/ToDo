e5d5c6db6d87821710dc377c71d4ae540b1feb1c
Tsuguma <htnod1429@gmail.com>
2018-11-05 02:40:38 +0900

    not-yet保存処理

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,37 @@
 let removeIcon = '<i class="far fa-trash-alt fa-lg"></i>';
 let doneIcon = '<i class="far fa-check-circle fa-lg"></i>';
-
+let data;
+//データ保存されていたが場合
+if (localStorage.getItem('todoList')){
+	data =JSON.parse(localStorage.getItem('todoList'));
+	for (let taskName of data.not){
+		addTaskToDOM(taskName);
+	}
+}else{
+	//データ保存されていない場合
+	data = {
+		not : [],
+		done : []
+	}
+}
+data = JSON.parse(localStorage.getItem('todoList'));
+console.log(data);
 
 // 追加ボタンをクリック
   let add = document.getElementById('add');
   add.addEventListener('click',function() {
     let taskName = document.getElementById('task').value;
-    let not = document.createElement('li');
+
+    document.getElementById('task').value = '';
+
+  //データ保存
+  data.not.push(taskName);
+  console.log(data);
+  localStorage.setItem('todoList',JSON.stringify(data));
+  });
+    
+  function addTaskToDOM(taskName){
+  	    let not = document.createElement('li');
     not.textContent = taskName;
     let buttons =document.createElement('div');
     buttons.classList.add('buttons');
@@ -16,7 +41,7 @@ let doneIcon = '<i class="far fa-check-circle fa-lg"></i>';
     remove.addEventListener('click',function(){
     	let task = this.parentNode.parentNode;
     	task.remove();
-    })
+    });
     let done = document.createElement('button');
     done.classList.add('done');
     done.innerHTML = doneIcon;
@@ -25,12 +50,10 @@ let doneIcon = '<i class="far fa-check-circle fa-lg"></i>';
     	console.log(task);
       let target = document.getElementById('done');
       target.appendChild(task);
-    })
+    });
     buttons.appendChild(remove);
     buttons.appendChild(done);
     not.appendChild(buttons);
     document.getElementById('not-yet').appendChild(not);
-    document.getElementById('task').value = '';
-
-  })
+  }
 
